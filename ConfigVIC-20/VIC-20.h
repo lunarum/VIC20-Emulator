34f1662b8859d1e63526503453c6d33d95db2608
lunarum <remco@lunarum.nl>
2017-09-27 12:59:53 +0200

    Video updated

---
+++ ConfigVIC-20/VIC-20.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "cpu.h"
+#include "memory.h"
+#include "vic.h"
+
+enum memory_extensions {
+    RAM_3K   =  1,
+    RAM_8K_1 =  2,
+    RAM_8K_2 =  4,
+    RAM_8K_3 =  8,
+    ROM_8K   = 16,
+};
+
+extern void load_rom_from_array(const unsigned *source, word address, word length);
+extern void vic20_config(unsigned extensions);
+
+#endif
